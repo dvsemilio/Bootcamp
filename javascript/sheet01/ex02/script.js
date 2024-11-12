e546918af5b4a5ad019c2557f3c74a5f482c880a
EmilioManana <emilio@gmail.com>
2024-11-12 11:19:10 +0100

    fix/js-sh01ex02-console-writing

---
+++ javascript/sheet01/ex02/script.js
@@ -0,0 +1,9 @@
+let number;
+let string;
+let boolean;
+number = 42;
+string = "Hola";
+boolean = true;
+console.log(number, typeof number);
+console.log(string, typeof string);
+console.log(boolean, typeof boolean);

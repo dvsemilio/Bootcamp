e721a2f459ca64e003dcc56942dd2f4efbb9e0e3
EmilioManana <emilio@gmail.com>
2024-11-12 12:28:47 +0100

    fix/sj-sh01ex03-maths-in-console

---
+++ javascript/sheet01/ex03/script.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+a = 10;
+b = 5;
+console.log("a es", a, "b es", b);
+console.log("suma:", a + b);
+console.log("resta:", a - b);
+console.log("multiplicación:", a * b);
+console.log("división:", a / b);
+console.log("resto:", a % b);

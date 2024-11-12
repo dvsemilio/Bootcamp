ff6e31792874e6d80bee2808e22e50c5e90dd381
EmilioManana <emilio@gmail.com>
2024-11-12 13:35:34 +0100

    js-sh01ex05-two-bucles-completed

---
+++ javascript/sheet01/ex05/script.js
@@ -0,0 +1,9 @@
+for (let numero = 0; numero < 11; numero++) {
+  console.log(numero);
+}
+let segundoNumero;
+segundoNumero = 0;
+while (segundoNumero < 11) {
+  console.log(segundoNumero);
+  segundoNumero++;
+}

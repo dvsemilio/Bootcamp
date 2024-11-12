985b351a03ca1bacc04d9d8864978232f897a9c9
EmilioManana <emilio@gmail.com>
2024-11-12 12:55:52 +0100

    fix/sh01ex04-checking-numbers-complete

---
+++ javascript/sheet01/ex04/script.js
@@ -0,0 +1,11 @@
+let numero;
+numero = "sadsa";
+if (numero == 0) {
+  console.log(`el número ${numero} es cero`);
+} else if (numero > 0) {
+  console.log(`el número ${numero} es positivo`);
+} else if (numero < 0) {
+  console.log(`el número ${numero} es negativo`);
+} else {
+  console.log(`${numero} no es un número pelotudo`);
+}

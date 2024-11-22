4e3be67cffd1711ce26520baf84a6fbeb6c07fb0
EmilioManana <emilio@gmail.com>
2024-11-22 10:04:07 +0100

    feat/react-sh01ex02-instalacion-comienzo

---
+++ react/sheet01/ex01/components/ListaTarjetas.jsx
@@ -0,0 +1,11 @@
+import Tarjeta from "./tarjeta";
+function ListaTarjeta() {
+  return (
+    <div>
+      <Tarjeta />
+      <Tarjeta />
+      <Tarjeta />
+    </div>
+  );
+}
+export default ListaTarjeta;
--- react/sheet01/ex01/src/App.jsx
+++ react/sheet01/ex01/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Tarjeta from "../components/tarjeta";
+import ListaTarjeta from "../components/ListaTarjetas";
 
 function App() {
   return (
     <>
       <Tarjeta
-        nombre="emilio"
-        apellido="apellido"
+        nombre={"emilio"}
+        apellido={"apellido"}
         ocupacion={"qweqew"}
         imagenURL={
           "https://images3.memedroid.com/images/UPLOADED916/60a3106a3c345.jpeg"
         }
       />
+      <ListaTarjeta nombre={"caraculo"} />
     </>
   );
 }

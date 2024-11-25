ef329179ed5acb3b411c7e6aa3bf85947d094d71
EmilioManana <emilio@gmail.com>
2024-11-25 10:38:19 +0100

    react-sh01ex02-product-list-complete

--- react/sheet01/ex02/components/ListaDeProductos.jsx
+++ react/sheet01/ex02/components/ListaDeProductos.jsx
@@ -1,1 +1,17 @@
+import Producto from "./producto";
+
+function ListaDeProductos({ array }) {
+  return (
+    <div className="Lista_De_Productos">
+      {array.map((Product, index) => (
+        <Producto
+          key={index}
+          nombre={Product.nombre}
+          precio={Product.precio}
+          descripcion={Product.descripcion}
+        />
+      ))}
+    </div>
+  );
+}
 export default ListaDeProductos;
--- react/sheet01/ex02/src/App.jsx
+++ react/sheet01/ex02/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
 import "./App.css";
-//import ListaDeProductos from "../components/ListaDeProductos";
-import Producto from "../components/producto";
+import ListaDeProductos from "../components/ListaDeProductos";
+const arrayDeProductos = [
+  {
+    nombre: "cerveza",
+    precio: "incalculable",
+    descripcion: "que cosa más rica",
+  },
+  { nombre: "galletas", precio: "1 euro", descripcion: "azucar pal cuerpo" },
+  { nombre: "cable", precio: "3 euros", descripcion: "conecta cosas" },
+  { nombre: "libro", precio: "12 euros", descripcion: "leer es bueno" },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Producto nombre={"mando"} precio={"5"} descripcion={"a distancia"} />
-      <Producto nombre={"cable"} precio={"6"} descripcion={"de 3 metros"} />
-      <Producto
-        nombre={"conector"}
-        precio={"7"}
-        descripcion={"de VGA a HDMI"}
-      />
+      <ListaDeProductos array={arrayDeProductos} />
     </>
   );
 }

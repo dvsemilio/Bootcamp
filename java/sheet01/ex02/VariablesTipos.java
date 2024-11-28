a9693ccf2ff70725497b40b8cb15a0eaa9010342
EmilioManana <emilio@gmail.com>
2024-11-28 12:13:56 +0100

    feat/java-sh01ex02-VariablesTipos-completado

---
+++ java/sheet01/ex02/VariablesTipos.java
@@ -0,0 +1,19 @@
+/*ex02: Variables y Tipos de Datos
+Declara variables de diferentes tipos de datos (int, double, boolean, char,
+String) y asígnales valores. Luego, imprime estos valores. Llama a la clase
+VariablesTipos . */
+public class VariablesTipos {
+
+    public static void main(String[] args) {
+        int edad = 30;
+        double salario = 1500.50;
+        boolean activo = true;
+        char inicial = 'J';
+        String palabra = "Juego";
+        System.out.println("el int es:"+edad);
+        System.out.println("el double es:"+salario);
+        System.out.println("el boolean es:"+activo);
+        System.out.println("el char es:"+inicial);
+        System.out.println("el String es:"+palabra);
+    }
+}

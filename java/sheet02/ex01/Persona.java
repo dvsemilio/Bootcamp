2f03aeb45e560fe6d77436fd9041884a4c1a65ed
EmilioManana <emilio@gmail.com>
2024-12-02 09:38:13 +0100

    feat/sh02ex01-Persona

---
+++ java/sheet02/ex01/Persona.java
@@ -0,0 +1,14 @@
+/*Ejercicio 1: Clase y Objeto
+Crea una clase Persona con los atributos nombre y edad (modificador default) y
+con un constructor para inicializar estos atributos. Añade un método
+mostrarDetalles para mostrar los detalles de la persona. Luego, crea un objeto
+de la clase llamando al constructor con nombre Juan y edad 25 en el main y
+muestra sus detalles llamando al método mostrarDetalles.
+Nombre: Juan
+Edad: 25 */
+public class Persona {
+    public static void main(String[] args) {
+        
+    }
+
+}
